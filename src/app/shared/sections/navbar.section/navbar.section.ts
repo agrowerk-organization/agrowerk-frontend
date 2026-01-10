29ee7fd165ad6837cf37156f57fce4306d632ab0
Douglas <douglasholanda3195@gmail.com>
2026-01-09 23:42:52 -0300

    chore: eslint added

---
+++ src/app/shared/sections/navbar.section/navbar.section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar.section',
+  imports: [],
+  templateUrl: './navbar.section.html',
+  styleUrl: './navbar.section.scss',
+})
+export class NavbarSection {
+
+}
